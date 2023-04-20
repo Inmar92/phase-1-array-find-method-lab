f76efcb0c0171ea6783797ac3b9d7b5ffefff064
inmar salinas <inmarsalinas@yahoo.com>
2023-04-19 18:47:09 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// code your solution here
+function superbowlWin(record){
+    let win = record.find(
+        function(Element){
+            if (Element.result === "W"){
+                return(Element);
+        }
+    })
+    if(win){
+        return win.year
+    }
+    else{
+        return undefined
+    }
+}
